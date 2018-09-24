16110f2301e09c10aab6a3bf23f706caa9a3601b
Fiona Wilson <fionaw237@gmail.com>
2018-09-24 20:18:53 +0100

    Completes printer class and tests

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,41 @@
 public class Printer {
+
+    private int sheets;
+    private int toner;
+
+    public Printer(int sheets, int toner){
+        this.sheets = sheets;
+        this.toner = toner;
+    }
+
+    public int getSheets() {
+        return sheets;
+    }
+
+    public int getToner() {
+        return toner;
+    }
+
+    public boolean isAbleToPrint(int pages, int copies){
+        return pages * copies <= this.sheets;
+    }
+
+    public void print(int pages, int copies){
+        if (isAbleToPrint(pages, copies)){
+            useSheets(pages, copies);
+            useToner(pages, copies);
+        }
+    }
+
+    public void useSheets(int pages, int copies){
+        this.sheets -= pages * copies;
+    }
+
+    public void useToner(int pages, int copies){
+        this.toner -= pages * copies;
+    }
+
+    public void refill(){
+        this.sheets = 100;
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,64 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer(100, 1000);
+    }
+
+    @Test
+    public void hasSheets(){
+        int sheets = printer.getSheets();
+        assertEquals(100, sheets);
+    }
+
+    @Test
+    public void ableToPrintIfEnoughPaper(){
+        boolean ableToPrint = printer.isAbleToPrint(10, 2);
+        assertEquals(true, ableToPrint);
+    }
+
+    @Test
+    public void notAbleToPrintIfNotEnoughPaper(){
+        boolean ableToPrint = printer.isAbleToPrint(51, 2);
+        assertEquals(false, ableToPrint);
+    }
+
+    @Test
+    public void printReducesSheetsByCopiesTimesPages(){
+        printer.print(10, 3);
+        assertEquals(70, printer.getSheets());
+    }
+
+    @Test
+    public void doesntPrintIfNotAbleTo(){
+        printer.print(10, 11);
+        assertEquals(100, printer.getSheets());
+    }
+
+    @Test
+    public void canRefillPrinter(){
+        printer.print(10, 3);
+        printer.refill();
+        assertEquals(100, printer.getSheets());
+    }
+
+    @Test
+    public void hasToner(){
+        int toner = printer.getToner();
+        assertEquals(1000, toner);
+    }
+
+    @Test
+    public void printReducesTonerBy1PerPage(){
+        printer.print(10, 3);
+        int newToner = printer.getToner();
+        assertEquals(970, newToner);
+    }
 }
