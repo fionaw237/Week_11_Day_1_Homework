aa9cdbbc8a7b2da74cb455ca00c58dd7e2b22c35
Fiona Wilson <fionaw237@gmail.com>
2018-09-24 19:40:55 +0100

    Completes water bottle class and tests

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,2 +1,27 @@
 public class WaterBottle {
+    private int volume;
+
+    public WaterBottle(int volume){
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void drink(){
+        this.volume -= 10;
+    }
+
+    public void empty(){
+        setVolume(0);
+    }
+
+    public void refill(){
+        setVolume(100);
+    }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume(){
+        int volume = waterBottle.getVolume();
+        assertEquals(100, volume);
+    }
+
+    @Test
+    public void drinkFunctionRemovesLiquid(){
+        waterBottle.drink();
+        int newVolume = waterBottle.getVolume();
+        assertEquals(90, newVolume);
+    }
+
+    @Test
+    public void canEmptyBottle(){
+        waterBottle.empty();
+        int newVolume = waterBottle.getVolume();
+        assertEquals(0, newVolume);
+    }
+
+    @Test
+    public void canRefillBottle(){
+        waterBottle.refill();
+        int newVolume = waterBottle.getVolume();
+        assertEquals(100, newVolume);
+    }
 }
