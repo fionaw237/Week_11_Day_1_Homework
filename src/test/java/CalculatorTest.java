3e18f84c884cda82aa6977f60cc7bae065f06b45
Fiona Wilson <fionaw237@gmail.com>
2018-09-24 19:21:25 +0100

    Completes calculator class and tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd(){
+        int result = calculator.add(3, 5);
+        assertEquals(8, result);
+    }
+
+    @Test
+    public void canSubtract(){
+        int result = calculator.subtract(3, 5);
+        assertEquals(-2, result);
+    }
+
+    @Test
+    public void canMultiply(){
+        int result = calculator.multiply(3, 5);
+        assertEquals(15, result);
+    }
+
+    @Test
+    public void canDivide(){
+        int result = calculator.divide(10, 5);
+        assertEquals(2, result);
+    }
 }
+
+
+
